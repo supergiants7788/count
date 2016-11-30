35181d2212badeb434ffd949c89b2fbf67d63c9d
新井山詠斗 <NiiyamaEito@macbook-pro.local>
2016-11-30 19:24:01 +0900

    Initial Commit

---
+++ count down3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  count down3
+//
+//  Created by 新井山詠斗 on 2016/11/30.
+//  Copyright © 2016年 新井山詠斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ count down3Tests/count_down3Tests.swift
@@ -0,0 +1,36 @@
+//
+//  count_down3Tests.swift
+//  count down3Tests
+//
+//  Created by 新井山詠斗 on 2016/11/30.
+//  Copyright © 2016年 新井山詠斗. All rights reserved.
+//
+
+import XCTest
+@testable import count_down3
+
+class count_down3Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
